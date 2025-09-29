b91a7719ccdeb995213fb7724adb5bf03844c08e
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2025-09-29 23:12:03 +0200

    test(search): fix ES Testcontainers config using DynamicPropertySource

--- Backend/Search/src/test/java/org/example/search/config/ElasticsearchTestConfig.java
+++ Backend/Search/src/test/java/org/example/search/config/ElasticsearchTestConfig.java
@@ -1,21 +1,25 @@
 package org.example.search.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 public class ElasticsearchTestConfig {
 
-    @Bean(destroyMethod = "stop")
-    public ElasticsearchContainer elasticsearchContainer() {
-        ElasticsearchContainer container = new ElasticsearchContainer(
-                DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.15.0")
-        ).withEnv("discovery.type", "single-node");
+    private static final ElasticsearchContainer container =
+            new ElasticsearchContainer(
+                    DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.15.0")
+            ).withEnv("discovery.type", "single-node");
 
+    static {
         container.start();
-        System.setProperty("spring.elasticsearch.uris", container.getHttpHostAddress());
+    }
 
-        return container;
+    @DynamicPropertySource
+    static void registerElasticsearchProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.elasticsearch.uris", container::getHttpHostAddress);
     }
 }
