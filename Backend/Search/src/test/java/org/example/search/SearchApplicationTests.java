9d264589671a202965ff2ad00af88388ef48de8d
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2025-09-29 23:08:17 +0200

    test(search): add app.properties with dummy ES, Kafka and Keycloak

--- Backend/Search/src/test/java/org/example/search/SearchApplicationTests.java
+++ Backend/Search/src/test/java/org/example/search/SearchApplicationTests.java
@@ -4,11 +4,12 @@ import org.example.search.config.ElasticsearchTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(ElasticsearchTestConfig.class)
+@ActiveProfiles("test")
 class SearchApplicationTests {
-
     @Test
     void contextLoads() {
     }
